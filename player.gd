03742684c75aed2886dcc8f98558e2f3ba50b1ce
UltimateBlackGuy <kcgermain14@gmail.com>
2026-05-19 19:59:57 -0400

    Made Basic Player Movement
    Added some changeable variables and speed variables to the player. Started work on anims but got called to dinner.

---
+++ player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+class_name Player
+
+@export var speed: int
+@export var health: int
+@export var Heat: int
+
+@onready var Sprite = $AnimatedSprite2D
+@onready var IdleAnim = Sprite.animation
+
+var WalkAnim = "Walking"
+#Fill this up when we actually have some of these guys
+var Alurians = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var dir = Input.get_vector("left", "right", "up", "down")
+	velocity = dir * speed
+	move_and_slide()
+	
+	print(velocity)
